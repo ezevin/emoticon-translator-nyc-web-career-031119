084acf76e94152e1caa65f309df9a65be3741df2
Elizabeth Zevin <ehzevin@gmail.com>
2019-02-25 17:40:45 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -43,18 +43,18 @@ end
 
 def get_english_meaning(library, emoticons)
   # code goes here
-  a = " "
- hash = load_library(library)
-  hash.each do |get_ , emoticon_hash|
-      if emoticon_hash.include?(emoticons) 
-        emoticon_hash.each do |english, japanese|
-          if english == emoticons
+#   a = " "
+# hash = load_library(library)
+#   hash.each do |get_ , emoticon_hash|
+#       if emoticon_hash.include?(emoticons) 
+#         emoticon_hash.each do |english, japanese|
+#           if english == emoticons
 
-          a =  hash["get_meaning"][japanese]
-          end 
-        end 
+#           a =  hash["get_meaning"][japanese]
+#           end 
+#         end 
    
-    end 
-  end 
-  return a
-end
+#     end 
+#   end 
+#   return a
+# end
