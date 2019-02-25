6f4451684144068c7d4d68f233a33b8cd4ea44f8
Elizabeth Zevin <ehzevin@gmail.com>
2019-02-25 17:09:00 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -48,7 +48,7 @@ def get_english_meaning(library, emoticons)
       if emoticon_hash.include?(emoticons) 
         emoticon_hash.each do |english, japanese|
         hash["get_meaning"].each do |x, meaning|
-          binding.pry
+         
           if english == emoticons && x.include?(japanese)
             return meaning
              end 
